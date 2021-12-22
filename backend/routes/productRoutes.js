1f4afd7a4883630cc6378febb17888b5335782d7
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-12-22 15:17:22 +0300

    Backend complete

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllProducts,
+  getProductById,
+} = require("../controller/productControllers");
+// get all products from db
+
+router.get("/",getAllProducts);
+
+// get a product  by id
+router.get("/:id", getProductById);
+
+module.exports = router;
